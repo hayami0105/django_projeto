419b8ab8e347b41179d5280820759f8364ca01cd
hayami0105 <marcelo.omosako@gamil.com>
2024-09-30 16:38:54 -0300

    commit 3009A

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Receitas',
+        })
 
 def contato(request):
     return render(request, 'recipes/contato.html')
