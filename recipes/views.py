619040aa2aaaf11ba66f71e42c1aa7f3d8fe51cc
hayami0105 <marcelo.omosako@gamil.com>
2024-10-18 16:47:46 -0300

    commit 1810

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,28 @@
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404, get_list_or_404, render
-from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 from django.db.models import Q
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
     ).order_by('-id')
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    paginator = Paginator(recipes, 2)
+    page_obj = paginator.get_page(current_page)
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 def category(request, category_id):
