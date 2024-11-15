85693531502a0440444738a89d73875fa19f2ab2
hayami0105 <marcelo.omosako@gamil.com>
2024-11-15 12:41:59 -0300

    commit1511

--- authors/views.py
+++ authors/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.http import Http404
-from django.shortcuts import redirect
+from django.contrib import messages
 
 def register_view(request):
-    #request.session['number']= request.session.get('number') or 1
-    #request.session['number']+= 1
     register_form_data= request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
@@ -18,4 +16,8 @@ def register_create(request):
     POST= request.POST
     request.session['register_form_data']= POST
     form = RegisterForm(POST)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+        del(request.session['register_form_data'])
     return redirect('authors:register')
