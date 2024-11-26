167c0f8308f95bb35d1519222d0950b7fc98cfb5
hayami0105 <marcelo.omosako@gamil.com>
2024-11-26 16:55:05 -0300

    commit 2611a

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
@@ -24,3 +26,10 @@ class Recipe(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('recipes:recipe', args=(self.id,))
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.title)}'
+            self.slug = slug
+        return super().save(*args, **kwargs)
